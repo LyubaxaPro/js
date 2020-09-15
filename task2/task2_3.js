56179f9fd7248bd36b200fdf4b43630ea2201b67
lyubaxapro <lyubaxova@mail.ru>
2020-09-15 17:55:59 +0300

    Add timer

--- task2/task2_3.js
+++ task2/task2_3.js
@@ -2,13 +2,22 @@
 
 let seconds = 0;
 let counter= 0;
-let delay = 1000;
 
-let interval = setInterval(() => {
+function f(delay){
+    let interval = setInterval(() => {
     seconds++;
     console.log(seconds);
-    if(seconds === 10) {
-        delay = 2000;
+    if(seconds === 10 || seconds === 20) {
+        if (seconds === 10){
+            delay = 1000;
+        }else {
+            delay = 2000;
+            seconds = 0;
+        }
         clearInterval(interval);
+        f(delay)
     }
-}, delay);
+    }, delay);
+}
+
+f(2000);
